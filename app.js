6dc4b28a0f5dcb73ab82a6250f6fc4fa2249e10b
surajit52 <surajitpaul57@gmail.com>
2023-04-19 14:09:58 +0530

    basic_app_ready_with_minion_api

--- app.js
+++ app.js
@@ -3,9 +3,31 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input")
 var outputDiv = document.querySelector("#output")
 
+//var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json?text=I am Surajit"
+
+
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
+
+function getTranslationURL(text){
+     return serverURL + "?" + "text=" + text
+}
+
+
+function errorHandler(error) {
+    console.log("error occured", error)
+    alert("something wrong witth server! try again after some time")
+}
 
 function clickHandler() {
-   outputDiv.innerText = "ajsajsajsajsajs " + txtInput.value;
+ 
+   var inputText = txtInput.value;
+   fetch(getTranslationURL(inputText))
+   .then(response => response.json())
+   .then(json => {
+    var translatedText = json.contents.translated;
+    outputDiv.innerText = translatedText;
+    })
+   .catch(errorHandler)
 };
 
 
